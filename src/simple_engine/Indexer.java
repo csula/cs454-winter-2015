ba870dbe7b8a1fdf4802393b0edd8431fd2ce4c9
John Tran <johnjtran@icloud.com>
2015-01-17 16:09:09 -0800

    move into correct directory

---
+++ src/simple_engine/Indexer.java
@@ -0,0 +1,10 @@
+package simple_engine;
+
+public class Indexer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/simple_engine/Storage.java
@@ -0,0 +1,12 @@
+package simple_engine;
+
+import java.io.File;
+import java.util.Map;
+
+public class Storage {
+
+	public static void save(File f, Map<String, String> metadata) {
+		
+	}
+
+}
