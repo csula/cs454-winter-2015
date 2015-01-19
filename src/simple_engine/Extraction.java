ba008c4e4335648857e3f638e6aa25dfcec532c6
John Tran <johnjtran@icloud.com>
2015-01-18 17:21:07 -0800

    Update Extraction.java

--- src/simple_engine/Extraction.java
+++ src/simple_engine/Extraction.java
@@ -7,6 +7,7 @@ public class Extraction {
 
 	public static Map<String, String> extract(File f) {
 		// TODO write the extractor as practice for homework...(not graded)
+		// recommendation: make a list of metadata that is relevant to a file and extract this information
 		return null;
 	}
 
